13b1c265bb593660052d2fcd45c0763069d0ddb9
DBroome00 <Daniel_Broome@outlook.com>
2022-10-24 11:49:24 +0200

    Moved  Sunnylands, Made Prefabs, Cleaned Code.

--- ProjectCutePsychos/Assets/Scripts/Player/PlayerMovement.cs
+++ ProjectCutePsychos/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,67 +4,125 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _rb2d;
 
-    public Rigidbody2D rb;
+    #region WalkingVariables
 
-    [Header ("Horizontal Movement")]
+    [Header("Horizontal Movement")]
+
     public float moveSpeed = 5f;
-    public float currentSpeed;
+
+    public float activeMoveSpeed;
+
     public float topSpeed;
+    #endregion
+
+    #region DashVariables
 
     [Header("Dashing")]
-    public bool canDash;
+
+    [SerializeField] private bool _canDash;
+
     public float dashSpeed;
-    public float dashLength = .5f, dashCoolDown = 1f;
+
+    public float dashLength = .5f;
+
+    public float dashCoolDown = 1f;
+
     private float dashCounter;
+
     private float dashCoolDownCounter;
+
     [SerializeField] private bool isDashing;
+    #endregion
 
+    #region GroundCheckVariables
+    [Header("Ground Check")]
 
-    [Header("Check if grounded")]
-    public bool isGrounded;
+    //ground
+
+    [SerializeField] private bool _isGrounded;
     public Transform groundCheck;
+
+    private float _checkGroundRadius;
+
     public LayerMask whatIsGround;
-    public float checkGroundRadius;
 
-    [Header("Jumping")]
+    #endregion
+
+    #region JumpVariables
+    [Header("Jump")]
+
+    //MultiJump
+
     public float jumpForce;
-    public float maxjumps;
-    public float availableJumps;
-    public bool canJump;
+
+    public bool canDoubleJump;
+
+    [SerializeField] private int _extraJumps;
+
+    #endregion
+
+    #region WallJumpVariables
+
+    [Header("Wall Check")]
+
+    //wall
+
+    [SerializeField] public static bool canWallSlide;
+
+    [SerializeField] public static bool canWallJump;
+
+    [SerializeField] private bool _isTouchingFront;
+
+    public Transform frontcheck;
+
+    [SerializeField] private bool _wallSliding;
+
+    [SerializeField] private float _checkWallRadius;
+
+    public float wallSlidingSpeed;
+
+    public LayerMask whatIsWall;
+
+    #endregion
 
 
-    private float inputX;
+    private float _moveInputX;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        currentSpeed = moveSpeed;
-        availableJumps = maxjumps;
+        dashSpeed = moveSpeed * 4;
+        activeMoveSpeed = moveSpeed;
+        _rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
 
     private void FixedUpdate()
     {
+        #region _isGrounded
         //check if the player is grounded and reset available jumps if the player is grounded
-        if (isGrounded == true)
+        if (_isGrounded == true && canDoubleJump == true)
         {
-            availableJumps = maxjumps;
-            canJump = true;
-        }   
+            _extraJumps = 1;
+        }
 
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 1f, whatIsGround);
+        _isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.35f, whatIsGround);
+        #endregion
 
+        #region Horizontal Walk
         //Move the player
-        rb.velocity = new Vector2(inputX * currentSpeed, rb.velocity.y);
+        _rb2d.velocity = new Vector2(_moveInputX * activeMoveSpeed, _rb2d.velocity.y);
+        #endregion
 
+        #region Dashing
         // If both the dash duration counter and the dash Cooldown is 0 or less the player can dash
         if (dashCoolDownCounter <= 0 && dashCounter <= 0)
         {
-            canDash = true;
-            isDashing = true;
+            _canDash = true;
         }
 
         //Dash duration countdown and reset currentSpeed to moveSpeed
@@ -74,10 +132,9 @@ public class PlayerMovement : MonoBehaviour
 
             if (dashCounter <= 0)
             {
-                currentSpeed = moveSpeed;
+                activeMoveSpeed = moveSpeed;
                 dashCoolDownCounter = dashCoolDown;
-                canDash = false;
-                isDashing = false;
+                _canDash = false;
             }
         }
         //Start the cooldown between dashes
@@ -85,51 +142,77 @@ public class PlayerMovement : MonoBehaviour
         {
             dashCoolDownCounter -= Time.deltaTime;
         }
+        #endregion
 
-        if (isDashing == true)
+        #region WallChcek
+        //Wall
+        _isTouchingFront = Physics2D.OverlapCircle(frontcheck.position, _checkWallRadius, whatIsWall);
+
+        if (_isTouchingFront == true && _isGrounded == false && _moveInputX != 0)
         {
-            
+            _wallSliding = true;
+
         }
+        else
+        {
+            _wallSliding = false;
+        }
+        if (_wallSliding)
+        {
+            _rb2d.velocity = new Vector2(_rb2d.velocity.x, Mathf.Clamp(_rb2d.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+
+        #endregion
     }
 
+    #region CallMoveFuntion
     //Call move function
     public void Move(InputAction.CallbackContext context)
     {
-        inputX = context.ReadValue<Vector2>().x;
+        _moveInputX = context.ReadValue<Vector2>().x;
     }
+    #endregion
 
+    #region CallJumpFunction
     //Call Jump Function
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed && canJump)
+        if (context.performed)
         {
-            if (availableJumps >= 0)
+            if (_extraJumps >= 0 && _isGrounded == true)
             {
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-                availableJumps --;
+                _rb2d.velocity = Vector2.up * jumpForce;
             }
-
-            if(availableJumps <= -1)
+            if (_extraJumps > 0 && _isGrounded == false)
             {
-                canJump = false;
+                _rb2d.velocity = Vector2.up * jumpForce;
+                _extraJumps--;
             }
         }
     }
+    #endregion
 
+    #region CallDashFunction
     //Call Dash funtion
     public void Dash(InputAction.CallbackContext context)
     {
-        if(context.performed && canDash)
+        if (context.performed && _canDash)
         {
-            currentSpeed = dashSpeed;
+            activeMoveSpeed = dashSpeed;
             dashCounter = dashLength;
         }
     }
+    #endregion
 
+    #region DrawGizmo
     //draw outline for groundcheck and wallcheck
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(groundCheck.position, checkGroundRadius);
+        Gizmos.DrawWireSphere(groundCheck.position, _checkGroundRadius);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(frontcheck.position, _checkWallRadius);
     }
+    #endregion
 }
