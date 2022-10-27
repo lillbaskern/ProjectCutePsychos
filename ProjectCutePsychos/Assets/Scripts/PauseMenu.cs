749236c963c9e911e6de4a3f93be1bdc5e045cdd
Hugo Axelsson <hugobax@yahoo.com>
2022-10-27 14:29:26 +0200

    Commented code

--- ProjectCutePsychos/Assets/Scripts/PauseMenu.cs
+++ ProjectCutePsychos/Assets/Scripts/PauseMenu.cs
@@ -16,30 +16,30 @@ public class PauseMenu : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (GameIsPaused)
+            if (GameIsPaused)   //Resumes game if game is paused and you press escape
             {
                 Resume();
 
             }
-            else Pause();
+            else Pause(); //Pauses the game
         }
     }
 
     public void Resume()
     {
-        pauseMenuUI.SetActive(false);
+        pauseMenuUI.SetActive(false); //If game is not paused resume time
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
 
     void Pause()
     {
-        pauseMenuUI.SetActive(true);
+        pauseMenuUI.SetActive(true); //If game is paused freeze time
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
 
-    public void LoadMenu()
+    public void LoadMenu() //Load Main Menu
     {
         SceneManager.LoadScene(0);
 
