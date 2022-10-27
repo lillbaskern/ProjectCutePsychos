1b90bfc308f216fe3f7326a253ef88a021dce2e7
lillbaskern <eliasfred8@gmail.com>
2022-10-27 15:16:45 +0200

    Functioning Scene Manager made

--- ProjectCutePsychos/Assets/Scripts/SceneController.cs
+++ ProjectCutePsychos/Assets/Scripts/SceneController.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneController : MonoBehaviour
+public static class SceneController
 {
-    int sceneCount;
-    [SerializeField] string[] scenes; 
-    void Start()
-    {
-        scenes = new string[sceneCount];
-        for (int i = 0; i < sceneCount; i++)
-        {
-            scenes[i] = SceneUtility.GetScenePathByBuildIndex(i);
-        }
+    //these enums should be very specific in how they want you to name things, so maybe just an array or list of strings would be the same in functionality but less complex
+    [SerializeField]public enum Scene{
+        MainMenu
+    }
+    public static void Load(Scene scene){
+        SceneManager.LoadScene(scene.ToString());
+    }
+    public static void LoadNext(){
+        var current = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(current.buildIndex + 1);
+    }
+    //<Summary>
+    //loads the current scene again. like loadnext but without the + 1
+    public static void Reload(){
+        var current = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(current.buildIndex);
     }
 
-    void Update()
-    {
-        
-    }
 }
