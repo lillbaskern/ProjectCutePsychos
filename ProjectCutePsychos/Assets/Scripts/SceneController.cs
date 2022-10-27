828067def2edd3b0f00c7597fd34f1ca9c78340a
lillbaskern <eliasfred8@gmail.com>
2022-10-27 15:54:55 +0200

    Accidentally fixed cam prefab in here :poop:
    Scene manager should be functioning fine now. Just dont try to load next when you are on the last scene in the build index

--- ProjectCutePsychos/Assets/Scripts/SceneController.cs
+++ ProjectCutePsychos/Assets/Scripts/SceneController.cs
@@ -5,16 +5,12 @@ using UnityEngine.SceneManagement;
 
 public static class SceneController
 {
-    //these enums should be very specific in how they want you to name things, so maybe just an array or list of strings would be the same in functionality but less complex
-    [SerializeField]public enum Scene{
-        MainMenu
-    }
     public static void Load(Scene scene){
         SceneManager.LoadScene(scene.ToString());
     }
     public static void LoadNext(){
-        var current = SceneManager.GetActiveScene();
-        SceneManager.LoadScene(current.buildIndex + 1);
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+            SceneManager.LoadScene(nextSceneIndex);
     }
     //<Summary>
     //loads the current scene again. like loadnext but without the + 1
