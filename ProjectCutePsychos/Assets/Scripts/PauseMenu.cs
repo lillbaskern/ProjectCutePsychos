cff18c5dedcf8d822eb833cfe18427d2645cba96
Hugo Axelsson <hugobax@yahoo.com>
2022-11-10 11:14:31 +0100

    Pause menu bug
    fix

--- ProjectCutePsychos/Assets/Scripts/PauseMenu.cs
+++ ProjectCutePsychos/Assets/Scripts/PauseMenu.cs
@@ -90,6 +90,7 @@ public class PauseMenu : MonoBehaviour
     public void Restart()
     {
         pauseMenuUI.SetActive(false);
+        GameIsPaused = false;
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
