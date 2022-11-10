7f5cf5f406e708d4030c3c97503487539324b2fa
DBroome00 <Daniel_Broome@outlook.com>
2022-11-10 11:35:48 +0100

    removed coinscollected from player

--- ProjectCutePsychos/Assets/Scripts/CoinsCollected.cs
+++ ProjectCutePsychos/Assets/Scripts/CoinsCollected.cs
@@ -6,8 +6,6 @@ public class CoinsCollected : MonoBehaviour
 {
     [SerializeField] private int value = 1;
 
-
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         //Check for the tag of collided object.
