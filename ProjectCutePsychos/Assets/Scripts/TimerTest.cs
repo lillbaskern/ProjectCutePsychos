3d507bd2e3c14c333315a41eeb926c230c16aa65
lillbaskern <114910143+lillbaskern@users.noreply.github.com>
2022-11-11 15:14:51 +0100

    Final Push! Woo hoo

--- ProjectCutePsychos/Assets/Scripts/TimerTest.cs
+++ ProjectCutePsychos/Assets/Scripts/TimerTest.cs
@@ -7,22 +7,15 @@ public class TimerTest : MonoBehaviour
     //int i = 0;
     int seconds;
     int minutes;
-
     TextMeshProUGUI text;
     void Start()
     {
         text = GameObject.Find("TimerText").GetComponent<TextMeshProUGUI>();
     }
-
     private void Update()
     {
         minutes = (int)Time.time / 60;
         seconds = (int)Time.time % 60;
         text.SetText($"{minutes}m{seconds}s");
-    }
-
-    void FixedUpdate()
-    {
-
     }
 }
